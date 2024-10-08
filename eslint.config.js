f31ca585c237b47f7e2f2019ef74ce935ff5df9e
barkingcat <kvl.olg@gmail.com>
2024-10-08 23:45:45 +0200

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,48 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'functional'],
-  extends: [
+module.exports = (async () => {
+  const typescriptEslintPlugin = await import(
+    '@typescript-eslint/eslint-plugin'
+  )
+  const functionalPlugin = await import('eslint-plugin-functional')
+
+  const typescriptEslintRecommended = await import(
+    '@typescript-eslint/eslint-plugin'
+  )
+  const functionalRecommended = await import(
+    'eslint-plugin-functional/configs/recommended'
+  )
+  const prettierConfig = await import('eslint-config-prettier')
+
+  return [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:functional/recommended',
-    'prettier',
-  ],
-  rules: {
-    // Customize rules based on functional programming principles
-    'functional/immutable-data': 'error',
-    'functional/no-let': 'error',
-    'functional/no-try-statement': 'error',
-    'functional/no-method-signature': 'error',
-    'functional/no-loop-statement': 'warn',
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/explicit-function-return-type': [
-      'error',
-      { allowExpressions: true },
-    ],
-  },
-}
+    typescriptEslintRecommended.configs.recommended,
+    functionalRecommended.default,
+    prettierConfig.default,
+    {
+      languageOptions: {
+        parser: '@typescript-eslint/parser',
+        ecmaVersion: 2023,
+        sourceType: 'module',
+      },
+      env: {
+        node: true,
+        es2023: true,
+      },
+      plugins: {
+        '@typescript-eslint': typescriptEslintPlugin.default,
+        functional: functionalPlugin.default,
+      },
+      rules: {
+        'functional/immutable-data': 'error',
+        'functional/no-let': 'error',
+        'functional/no-try-statement': 'error',
+        'functional/no-method-signature': 'error',
+        'functional/no-loop-statement': 'warn',
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/explicit-function-return-type': [
+          'error',
+          { allowExpressions: true },
+        ],
+      },
+    },
+  ]
+})()
